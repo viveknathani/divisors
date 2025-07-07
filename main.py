5445c6fa2b4c1bcc8c117c0f4003b2b17fbe89bf
viveknathani <viveknathani2402@gmail.com>
2025-07-07 15:38:45 +0530

    feat: abundancy index

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import matplotlib.animation as animation
 def divisor_sum(n, k):
     return sum([i**k for i in range(1, n + 1) if n % i == 0])
 
-def main():
+def abundancy_index(n):
+    return divisor_sum(n, 1) / n
+
+def plot_divisor_sums():
     n = 250
     k_values = list(range(1, 8))
     n_values = list(range(1, n + 1))
@@ -36,7 +39,18 @@ def main():
     
     _ = animation.FuncAnimation(figure, update, frames=len(k_values), interval=1000, repeat=False, init_func=init)
     plt.show()
+
+def plot_abundancy_index():
+    n = 5_000
+    n_values = list(range(1, n + 1))
+    abundancy_indices = [abundancy_index(i) for i in n_values]
     
+    plt.scatter(n_values, abundancy_indices)
+    plt.title(f"abundancy index = $\\sigma_1(n)/n$")
+    plt.show()
+
+def main():
+    plot_abundancy_index()
 
 
 if __name__ == "__main__":
